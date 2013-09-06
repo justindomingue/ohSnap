287fa92f236a0b4b51d020cce429dfb474a9f23c
Justin Domingue <justin.domingue@hotmail.com>
2013-09-06 16:22:22 -0400

    Added OhSnap to the Github pages

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,5 @@
-console.log('This would be the main JS file.');
+$('#red').on('click', function() { ohSnap('Oh Snap! You can\'t access this page!', 'red')});
+$('#green').on('click', function() { ohSnap('Ahh Yeah! Your account was created.', 'green')});
+$('#blue').on('click', function() { ohSnap('We are closed right now. Come back tomorrow.', 'blue')});
+$('#yellow').on('click', function() { ohSnap('A yellow alert for your... yellow needs...', 'yellow')});
+$('#orange').on('click', function() { ohSnap('Free for all!', 'orange')});
---
+++ javascripts/ohsnap.js
@@ -0,0 +1,46 @@
+/**
+ * == OhSnap!.js ==
+ * A simple notification jQuery/Zepto library designed to be used in mobile apps
+ *
+ * author: Justin Domingue
+ * date: september 5, 2013
+ * version: 0.1.2
+ * copyright - nice copyright over here
+ */
+
+function ohSnap(text, color) {
+  // text : message to show (HTML tag allowed)
+  // Available colors : red, green, blue, orange, yellow --- add your own!
+  
+  // Set some variables
+  var time = '1000';
+  var $container = $('#ohsnap');
+
+  // Generate the HTML
+  var html = '<div class="alert alert-' + color + '">' + text + '</div>';
+
+  // Append the label to the container
+  $container.append(html);
+  
+  // After 'time' seconds, the animation fades out
+  setTimeout(function () {
+    ohSnapX($container.children('.alert').last());
+  }, time);
+}
+
+function ohSnapX(element) {
+  // Called without argument, the function removes all alerts
+  // element must be a jQuery object
+  
+  if (typeof element !== "undefined" ) {
+    element.remove();
+  } else {
+    $('.alert').remove();
+  }
+}
+
+// Remove the notification on click
+
+$('.alert').live('click', function() { 
+  ohSnapX($(this))
+});
