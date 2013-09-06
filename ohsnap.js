5503dc867adfb70c10c5ac9c23bafaa47a6d400b
Justin Domingue <justin.domingue@hotmail.com>
2013-09-06 15:26:18 -0400

    Removed animate.css animation, and add support for stacking

--- ohsnap.js
+++ ohsnap.js
@@ -11,38 +11,36 @@
 function ohSnap(text, color) {
   // text : message to show (HTML tag allowed)
   // Available colors : red, green, blue, orange, yellow --- add your own!
-  // container : TODO
   
   // Set some variables
-  var in_animation = 'bounceInUp';
-  var time = '5000';
-  
-  // Remove any previous notificaiton
-  ohSnapX();
-  
+  var time = '1000';
+  var $container = $('#ohsnap');
+
   // Generate the HTML
-  var html = '<div class="animated ' + in_animation + ' alert alert-' + color + '">' + text + '</div>';
-  
+  var html = '<div class="alert alert-' + color + '">' + text + '</div>';
+
   // Append the label to the container
-  $('#content').append(html)
+  $container.append(html);
   
   // After 'time' seconds, the animation fades out
   setTimeout(function () {
-    ohSnapX();
+    ohSnapX($container.children('.alert').last());
   }, time);
 }
 
-function ohSnapX() {
+function ohSnapX(element) {
+  // Called without argument, the function removes all alerts
+  // element must be a jQuery object
   
-  var out_animation = 'bounceOutDown'
-  
-  // Removes the alert from the page
-  $('.alert').addClass(out_animation);
+  if (typeof element !== "undefined" ) {
+    element.remove();
+  } else {
+    $('.alert').remove();
+  }
 }
 
 // Remove the notification on click
-// TODO: change click to tap
 
 $('.alert').live('click', function() { 
-  ohSnapX();
+  ohSnapX($(this))
 });
