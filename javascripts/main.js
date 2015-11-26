b616caa23bcecbbfac5e5e842ecbe045ef7faefc
justindomingue <justin.domingue@hotmail.com>
2015-11-26 13:41:49 -0500

    adapted for version 1.0.0

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,6 @@
-$('#red').on('click', function() { ohSnap('Oh Snap! You can\'t access this page!', 'red')});
-$('#green').on('click', function() { ohSnap('Ahh Yeah! Your account was created.', 'green')});
-$('#blue').on('click', function() { ohSnap('We are closed right now. Come back tomorrow.', 'blue')});
-$('#yellow').on('click', function() { ohSnap('A yellow alert for your... yellow needs...', 'yellow')});
-$('#orange').on('click', function() { ohSnap('Free for all!', 'orange')});
+$('#default').on('click', function() { ohSnap('A notification with default options')});
+$('#red').on('click', function() { ohSnap('Oh Snap! You can\'t access this page!', {'color':'red'})});
+$('#green').on('click', function() { ohSnap('Ahh Yeah! Your account was created.', {'color':'green'})});
+$('#blue').on('click', function() { ohSnap('We are closed right now. Come back tomorrow.', {'color':'blue'})});
+$('#yellow').on('click', function() { ohSnap('A yellow alert for your... yellow needs...', {color:'yellow'})});
+$('#orange').on('click', function() { ohSnap('A fast one', {'color': 'orange', 'duration':'100'})});
